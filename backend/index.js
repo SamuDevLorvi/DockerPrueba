084422b5399bc2682dd4976c3303db2e0f937840
samuel.lorvi@gmail.com <samuel.lorvi@gmail.com>
2025-05-20 12:52:37 +0200

    2.0.0

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,29 @@
 import express from "express";
 import cors from "cors";
 import db from "mysql2/promise";
+import "dotenv/config";
+// import bodyParser from "body-parser";
+// import routes from "./routes";
 
 const app = express();
 const port = 3000;
 app.use(cors());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// const router = require("./routes");
+// app.use("/api", router);
+
 let connection;
+
 try {
-	console.log("Connection");
-	console.log("process.env.DB_NAME", process.env.DB_NAME);
 	connection = await db.createConnection({
-		host: "mysql",
+		host: process.env.DB_HOST,
+		// host: "localhost",
+		database: process.env.DB_NAME,
+		// database: "mydatabase",
 		user: "root",
 		password: "my-secret-pw",
 		port: 3306,
-		database: process.env.DB_NAME,
 	});
 	console.log("Connection success");
 } catch (error) {
@@ -23,15 +32,11 @@ try {
 
 //Ruta de prueba
 app.get("/", async (req, res) => {
-	try {
-		console.log("hola");
-		const [results] = await connection.query("SELECT * FROM usuario;");
-
-		res.status(200).send(results);
-	} catch (error) {
-		console.error(error);
-		res.status(500).send("Error al conectar con la base de datos");
-	}
+	const [results] = await connection.query("SELECT * FROM usuario;");
+	res.send(results);
+});
+app.get("/login", async (req, res) => {
+	console.log("login hecho");
 });
 
 app.listen(port, () => {
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,16 +3,13 @@ import { createApp, ref, onMounted } from "vue";
 const App = {
 	setup() {
 		const message = ref("Cargando...");
-
 		onMounted(async () => {
 			console.log("HI");
-			const res = await fetch('http://localhost:3000/');
-			console.log("res",res);
-			
+			const res = await fetch("http://localhost:3000/");
+			console.log("res", res);
 			const data = await res.json();
 			message.value = data.message;
 		});
-
 		return { message };
 	},
 	template: `<h1>{{ message }}</h1>`,
